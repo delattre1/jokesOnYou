ecece482cfa35565459e08c4c063c54ebfe13b85
delattre1 <daniieeldelattre@gmail.com>
2021-04-29 19:52:40 -0300

    removed react from inside of django project

--- jokesOnYou/jokesOnYou/settings.py
+++ jokesOnYou/jokesOnYou/settings.py
@@ -21,7 +21,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'jokes.apps.JokesConfig',
-    'frontend.apps.FrontendConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
