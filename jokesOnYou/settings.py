579f52dea765bfc561c999f93ff1e9f2ee27936e
delattre1 <daniieeldelattre@gmail.com>
2021-05-04 19:14:11 -0300

    back to sqlite3 to see if heroku works

--- jokesOnYou/settings.py
+++ jokesOnYou/settings.py
@@ -1,4 +1,3 @@
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -67,23 +66,23 @@ WSGI_APPLICATION = 'jokesOnYou.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'jokes',
-        'USER': 'jokesuser',
-        'PASSWORD': 'abc123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'jokes',
+#        'USER': 'jokesuser',
+#        'PASSWORD': 'abc123',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+# }
 
 
 # Password validation
