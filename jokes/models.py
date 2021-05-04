c0d761931b0fe4139060d5a057c11a86431dc71f
delattre1 <daniieeldelattre@gmail.com>
2021-05-04 17:42:27 -0300

    removed some field, and worked again (database)

--- jokes/models.py
+++ jokes/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Joke(models.Model):
     setup = models.CharField(max_length=300)
     delivery = models.CharField(max_length=300)
-    joke_id = models.IntegerField()
 
     def __str__(self):
         return f'{self.id}, {self.setup}'
--- jokes/views.py
+++ jokes/views.py
@@ -29,9 +29,8 @@ def getJoke():
 
     setup = response['setup']
     delivery = response['delivery']
-    joke_id = response['id']
 
-    return setup, delivery, joke_id
+    return setup, delivery
 
 
 def translateJoke(joke):
@@ -53,13 +52,13 @@ def translateJoke(joke):
 @api_view(['GET', 'POST'])
 def api_joke(request):
 
-    setup, delivery, joke_id = getJoke()
+    setup, delivery = getJoke()
 
     with concurrent.futures.ThreadPoolExecutor() as executor:
         setup_pt = executor.submit(translateJoke, setup).result()
         delivery_pt = executor.submit(translateJoke, delivery).result()
 
-    joke = Joke(setup=setup_pt, delivery=delivery_pt, joke_id=joke_id)
+    joke = Joke(setup=setup_pt, delivery=delivery_pt)
     serialized_joke = JokeSerializer(joke)
 
     return Response(serialized_joke.data)
@@ -71,7 +70,7 @@ def api_favorites(request):
         new_fav_data = request.data
         print(f'data post: {new_fav_data}')
         new_fav = Joke(setup=new_fav_data['setup'],
-                       delivery=new_fav_data['delivery'], joke_id=new_fav_data['joke_id'])
+                       delivery=new_fav_data['delivery'])
         new_fav.save()
 
     all_favs = Joke.objects.all()
