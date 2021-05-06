873ed97d94783d1cd2c2216dacf6d81b7ae55448
delattre1 <daniieeldelattre@gmail.com>
2021-05-06 10:17:29 -0300

    fixed database postgresql

--- jokesOnYou/settings.py
+++ jokesOnYou/settings.py
@@ -13,9 +13,6 @@ SECRET_KEY = 'django-insecure-qe6uwzz$%fs-n6p%n39qgm6a8!tbnfjx&@2!=129_g(-t9g_6k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -66,23 +63,23 @@ WSGI_APPLICATION = 'jokesOnYou.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'jokes',
-#        'USER': 'jokesuser',
-#        'PASSWORD': 'abc123',
-#        'HOST': 'localhost',
-#        'PORT': '5432',
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'jokes',
+        'USER': 'jokesuser',
+        'PASSWORD': 'abc123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
